746f537fd365e3614120dac11cca05ee399dbc8e
pae_adwaa <adu27747g@gmail.com>
2025-04-03 17:55:59 +0700

    Inset DB

---
+++ Back-End/33. PostgreSQL/33.3 Travel Tracker/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import e from "express";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "root",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  const countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
+});
+
+app.post("/add", async (req, res) => {
+  const country = req.body["country"];
+  const country_code = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [country]);
+
+  if (country_code.rows.length !== 0) {
+    const data = country_code.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
